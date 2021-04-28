e14b41ffdb4bee4f93a9b2cf7c0407cba7f9d959
Ahmed Abuelfateh <abuelfateh@yahoo.com>
2021-04-28 22:49:26 +0200

    Initial commit

---
+++ Abuelfateh.h
@@ -0,0 +1,12 @@
+#ifndef ABUELFATEH_H
+#define ABUELFATEH_H 1
+
+#include <cstdlib>
+#include <cstdio>
+#include <conio.h>
+#include <cmath>
+#include <cstring>
+#include <Windows.h>
+
+
+#endif // !ABUELFATEH_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "Abuelfateh.h"
+
+int main(int argc, char *argv[]) {
+
+
+
+  printf("\nDeveloped by Ahmed Abuelfateh...");
+  (void)_getch();
+  return 0;
+}
